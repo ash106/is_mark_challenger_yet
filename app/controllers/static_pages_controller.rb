8cfc8c2ecb400b3802be26c0d71e2a5294b10bfc
Alex Howington <alex.howington@gmail.com>
2016-05-08 14:57:25 -0500

    Rescue 404 error if Mark is unranked

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,14 +4,20 @@ class StaticPagesController < ApplicationController
   def home
     client = Lol::Client.new ENV["RIOT_API_KEY"], {region: "na"}
     summoner_id = client.summoner.by_name("telegonies75")[0].id
-    league_entries = client.league.get_entries(summoner_id)
-    league_entries["#{summoner_id}"].each do |entry|
-      if entry.queue == "RANKED_SOLO_5x5"
-        @tier = entry.tier.capitalize
-        @division = entry.entries[0].division
-        @points = entry.entries[0].league_points
-        @answer = @tier == "Challenger" ? "Yes!" : "No."
+    begin
+      league_entries = client.league.get_entries(summoner_id)
+      league_entries["#{summoner_id}"].each do |entry|
+        if entry.queue == "RANKED_SOLO_5x5"
+          @tier = entry.tier.capitalize
+          @division = entry.entries[0].division
+          @points = entry.entries[0].league_points
+          @answer = @tier == "Challenger" ? "Yes!" : "No."
+        end
       end
+    rescue
+      @tier = "Unranked"
+      @points = "0"
+      @answer = "No."
     end
   end
 end
